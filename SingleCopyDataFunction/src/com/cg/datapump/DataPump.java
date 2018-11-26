1d632acd35efec1d41f80bf742b5dc66e78a7206
masivali <masivali@DIN13002984.corp.capgemini.com>
2018-11-26 19:23:26 +0530

    FirstCommit

---
+++ SingleCopyDataFunction/src/com/cg/datapump/DataPump.java
@@ -0,0 +1,39 @@
+package com.cg.datapump;
+
+public class DataPump {
+	private Connection sourceConnection;
+	  private Connection destConnection;
+	  private Table table;
+	  
+	  public Connection getSourceConnection()
+	  {
+	    return this.sourceConnection;
+	  }
+	  
+	  public void setSourceConnection(Connection sourceConnection)
+	  {
+	    this.sourceConnection = sourceConnection;
+	  }
+	  
+	  public Connection getDestConnection()
+	  {
+	    return this.destConnection;
+	  }
+	  
+	  public void setDestConnection(Connection destConnection)
+	  {
+	    this.destConnection = destConnection;
+	  }
+
+	public Table getTable() {
+		return table;
+	}
+
+	public void setTable(Table table) {
+		this.table = table;
+	}
+
+	
+	  
+	 
+}
